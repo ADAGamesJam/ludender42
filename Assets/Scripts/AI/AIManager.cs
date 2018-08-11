fa0b5047a43b61501329b1c2589f2f40ac37ebbb
Gennadii Movila <movila745@gmail.com>
2018-08-11 20:18:23 +0300

    Ai structure

---
+++ Assets/Scripts/AI/AIManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIManager : MonoBehaviour
+{
+    public List<Enemy> enemyList;
+    public Renderer renderManager;
+	void Start ()
+	{
+	    renderManager = GetComponent<Renderer>();
+	}
+	
+	
+	void Update () {
+	    if (renderManager.isVisible)
+	    {
+	        foreach (var enemy in enemyList)
+	        {
+	           enemy.DoMove(enemy);
+	        }
+	    }
+	}
+
+    
+}
---
+++ Assets/Scripts/AI/Enemy.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class Enemy : MonoBehaviour
+{
+    private HeroControlls hero;
+    [Header("References")]
+    [Tooltip("Любай тайлмап. Он будет дергать из нее размер клетки")]
+    public Tilemap tilemap;
+    [HideInInspector]
+    public float tileSize;
+
+    private Vector3 direction;
+    private LayerMask maskCombined = 1 << 8 | 1 << 9;
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+
+    public void DoMove(Enemy enemy)
+    {
+        if (!hero.heroMoved)
+        {
+            if (!Physics2D.Raycast(transform.position, Vector3.down * tileSize, 1f, maskCombined.value))
+            {
+                transform.Translate(Vector3.down * tileSize);
+                //CheckSurroundings();
+                //direction = Vector3.down;
+            }
+        }
+    }
+}
