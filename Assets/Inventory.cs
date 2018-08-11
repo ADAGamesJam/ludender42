087e99e212e8f51802276c36a72cb1b43da7b399
Demon465268 <pokidovdima@mail.ru>
2018-08-11 20:08:39 +0300

    Added Bagpack Scene

---
+++ Assets/Inventory.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Inventory : MonoBehaviour {
+
+    public static Inventory instance;
+
+    public GameObject Slot1;
+    public GameObject Slot2;
+
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        if (instance != this)
+            Destroy(this);
+    }
+
+    public string[] SlotInfo()
+    {
+        string[] inventoryInfo = new string[2];
+
+        if (Slot1.gameObject.transform.GetChild(0) != null)
+        {
+            inventoryInfo[0] = Slot1.gameObject.transform.GetChild(0).name;
+        }
+        else
+            inventoryInfo[0] = "";
+
+        if (Slot2.gameObject.transform.GetChild(0) != null)
+        {
+            inventoryInfo[1] = Slot1.gameObject.transform.GetChild(0).name;
+        }
+        else
+            inventoryInfo[1] = "";
+
+        return inventoryInfo;
+        }
+
+    }
+
