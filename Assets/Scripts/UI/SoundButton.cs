170404dadd80c2e18ca59917f0e7e9b13c30023e
Demon465268 <pokidovdima@mail.ru>
2018-08-12 22:12:03 +0300

    Part of GUI added

--- Assets/Scripts/UI/SoundButton.cs
+++ Assets/Scripts/UI/SoundButton.cs
@@ -5,43 +5,68 @@ using UnityEngine.UI;
 
 public class SoundButton : MonoBehaviour {
 
+
+
     public GameObject soundButton;
     private Image image;
     public Sprite sprite1;
+    public Sprite sprite1Hltd;
     public Sprite sprite2;
+    public Sprite sprite2Hltd;
+
+
+    public static SoundButton instance = null;
+    [HideInInspector]
+    public bool isSound;
 
     private void Awake()
     {
         image = soundButton.GetComponent<Image>();
-
+        isSound = true;
     }
 
-    public bool isSound = true;
+    private void Start()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(gameObject);
+    }
+
+
 
     public void soundSwitch()
     {
-
         if (isSound == true)
         {
             isSound = false;
-
         }
-        else
+        else 
             isSound = true;
 
     }
 
-    private void Update()
+    public void Update()
     {
         if(isSound == true)
         {
-            image.color = Color.red;
-
+            SpriteState s1h = new SpriteState();
+            image.sprite = sprite1;
+            s1h.highlightedSprite = sprite1Hltd;
+            
         }
         else
         {
+            SpriteState s2h = new SpriteState();
+            image.sprite = sprite2;
+            s2h.highlightedSprite = sprite2Hltd;
 
-            image.color = Color.black;
+
         }
             
     }
