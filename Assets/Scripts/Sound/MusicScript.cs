9844cb04d060719c98729c02b768ba3d5693434b
Demon465268 <pokidovdima@mail.ru>
2018-08-13 04:27:22 +0300

    Prefabs added

---
+++ Assets/Scripts/Sound/MusicScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicScript : MonoBehaviour {
+
+    public AudioClip musicClip1;
+    public AudioClip musicClip2;
+
+    public AudioSource musicSource;
+	// Use this for initialization
+	void Start () {
+        musicSource.clip = musicClip1;
+        //musicSource.clip = musicClip2;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!SoundButton.instance.isSound)  //&& SceneManager.GetActiveScene().name == "MainMenu")
+        {
+            musicSource.clip = musicClip1;
+            musicSource.Play();
+
+        }
+        /*if(SoundButton.instance.isSound ) //&& SceneManager.GetActiveScene().name == "SampleScene"){
+        {
+            musicSource.clip = musicClip2;
+            musicSource.Play();
+
+        }*/
+
+	}
+}
---
+++ Assets/Scripts/UI/PerkTextController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PerkTextController : MonoBehaviour
+{
+
+    public enum ImgType
+    {
+        img_1, img_2, img_3
+    }
+
+    public Image slot1;
+    public Image slot2;
+    public Image slot3;
+    public Image image;
+    public Text text;
+    public GameObject plate;
+    public ImgType type;
+
+    public void Enter()
+    {   
+        if(image.transform.parent.name == slot1.name || image.transform.parent.name == slot2.name || image.transform.parent.name == slot3.name)
+        {   
+            plate.SetActive(true);
+
+            /*if (type == ImgType.img_1)
+                text.text = "1";
+            else
+            if (type == ImgType.img_2)
+                text.text = "2";
+             else
+            if (type == ImgType.img_3)
+                text.text = "3";
+            Debug.Log(gameObject.name);*/
+        }
+    }
+
+    public void Exit()
+    {
+
+        plate.SetActive(false);
+       // text.SetActive(false);
+    }
+
+}
