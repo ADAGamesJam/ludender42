9454cec22493ea20f46fac4149bca4e65e004914
Gennadii Movila <movila745@gmail.com>
2018-08-13 20:49:13 +0300

    Add enemyController

--- Assets/Scripts/Dan/EnemyController.cs
+++ Assets/Scripts/Dan/EnemyController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
@@ -31,32 +32,84 @@ public class EnemyController : MonoBehaviour
         {
             Vector3 dir = (HeroControlls.instance.gameObject.transform.position - transform.position);
 
-            if (HeroControlls.instance.keyList.Last() == 1)
+            Debug.Log(HeroControlls.instance.keyList.Last());
+
+            if ((HeroControlls.instance.keyList.Last() == 1 || HeroControlls.instance.keyList.Last() == 2)
+                && (Math.Abs(transform.position.y - HeroControlls.instance.transform.position.y)) < 0.5
+                && transform.position.x < HeroControlls.instance.transform.position.x)
             {
                 //if (dir.x > 0)
                 //{
+                HeroControlls.instance.moveFlag = false;
                 moveInst = MoveInst.right;
-                HeroControlls.instance.keyList.Remove(1);
+                if (HeroControlls.instance.keyList.Last() == 2) HeroControlls.instance.keyList.Remove(2);
+                else if (HeroControlls.instance.keyList.Last() == 1) HeroControlls.instance.keyList.RemoveAll(item => item == 1);
+                HeroControlls.instance.moveFlag = true;
                 //}
             }
-            else if (HeroControlls.instance.keyList.Last() == 2)
+            else if ((HeroControlls.instance.keyList.Last() == 1 || HeroControlls.instance.keyList.Last() == 2) 
+                     && (Math.Abs(transform.position.y - HeroControlls.instance.transform.position.y)) < 0.5
+                     && transform.position.x > HeroControlls.instance.transform.position.x)
             {
+                HeroControlls.instance.moveFlag = false;
                 moveInst = MoveInst.left;
-                HeroControlls.instance.keyList.Remove(2);
+                if(HeroControlls.instance.keyList.Last() == 2 ) HeroControlls.instance.keyList.Remove(2);
+                else if (HeroControlls.instance.keyList.Last() == 1) HeroControlls.instance.keyList.RemoveAll(item => item == 1);
+                HeroControlls.instance.moveFlag = true;
             }
-            else if (HeroControlls.instance.keyList.Last() == 3)
+            else if ((HeroControlls.instance.keyList.Last() == 3 || HeroControlls.instance.keyList.Last() == 4) 
+                && (Math.Abs(transform.position.x - HeroControlls.instance.transform.position.x)) < 0.5
+                     && transform.position.y < HeroControlls.instance.transform.position.y)
             {
+                HeroControlls.instance.moveFlag = false;
                 moveInst = MoveInst.up;
-                HeroControlls.instance.keyList.Remove(3);
+                if (HeroControlls.instance.keyList.Last() == 3) HeroControlls.instance.keyList.Remove(3);
+                else if (HeroControlls.instance.keyList.Last() == 4) HeroControlls.instance.keyList.RemoveAll(item => item == 4);
+                HeroControlls.instance.moveFlag = true;
             }
 
-            else if (HeroControlls.instance.keyList.Last() == 4)
+            else if ((HeroControlls.instance.keyList.Last() == 3 || HeroControlls.instance.keyList.Last() == 4)
+                     && (Math.Abs(transform.position.x - HeroControlls.instance.transform.position.x)) < 0.5
+                     && transform.position.y > HeroControlls.instance.transform.position.y)
             {
+                HeroControlls.instance.moveFlag = false;
                 moveInst = MoveInst.down;
-               Debug.Log(HeroControlls.instance.keyList.Last());
-                HeroControlls.instance.keyList.Remove(4);
+                HeroControlls.instance.keyList.Remove(3);
+                if (HeroControlls.instance.keyList.Last() == 3) HeroControlls.instance.keyList.Remove(3);
+                else if (HeroControlls.instance.keyList.Last() == 4) HeroControlls.instance.keyList.RemoveAll(item => item == 4);
+                HeroControlls.instance.moveFlag = true;
             }
+            
+           // DistanceMove();
         }
+    }
+
+    public void DistanceMove()
+    {
+        if ((HeroControlls.instance.transform.position.x > transform.position.x 
+             && Math.Abs(transform.position.x - HeroControlls.instance.transform.position.x) > 0.5))
+        {
+            Debug.Log("Piska1");
+            moveInst = MoveInst.up;
+        }
+        else if (HeroControlls.instance.transform.position.y > transform.position.y 
+                 && Math.Abs(transform.position.y - HeroControlls.instance.transform.position.y) > 0.5)
+        {
+            Debug.Log("Piska2");
+            moveInst = MoveInst.down;
+        }
+        /*else if (HeroControlls.instance.transform.position.y < transform.position.y 
+                 && Math.Abs(transform.position.y - HeroControlls.instance.transform.position.y) > 0.5)
+        {
+            Debug.Log("Piska3");
+            moveInst = MoveInst.down;
+        }
+        else if (HeroControlls.instance.transform.position.x < transform.position.x 
+                 && Math.Abs(transform.position.x - HeroControlls.instance.transform.position.x) > 0.5)
+        {
+            Debug.Log("Piska4");
+            moveInst = MoveInst.left;
+        }*/
     }
 
     public void Move()
