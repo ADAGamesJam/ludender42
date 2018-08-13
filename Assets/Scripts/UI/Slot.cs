decdfdc824251cf899e846cdc22dcdaf2dcdddf6
Demon465268 <pokidovdima@mail.ru>
2018-08-13 04:24:24 +0300

    Upgrades added

--- Assets/Scripts/UI/Slot.cs
+++ Assets/Scripts/UI/Slot.cs
@@ -19,7 +19,89 @@ public class Slot : MonoBehaviour , IDropHandler {
     }
 
     public void OnDrop(PointerEventData eventData)
-    {
+    {   if(this.gameObject.transform.childCount < 1 )
         DragHandler.itemBeingDragged.transform.SetParent(transform);
+    }
+
+    public void Update()
+    {
+
+        // Upgrate Fight
+        if (transform.childCount > 0 && (gameObject.name == "Slot1" || gameObject.name == "Slot2"))
+        {
+            
+            if (transform.GetChild(0).name == "Image")
+            {
+                HeroControlls.instance.upgradeFight = true;
+
+            }
+
+                
+        }
+        if (transform.childCount > 0 && (gameObject.name == "Slot3" || gameObject.name == "Slot4" || gameObject.name == "Slot5"))
+        {
+            if (transform.childCount > 0)
+            {
+                if (transform.GetChild(0).name == "Image")
+                    HeroControlls.instance.upgradeFight = false;
+            }
+
+        }
+
+
+
+
+
+        // Upgrade Hack
+        if (transform.childCount > 0 && (gameObject.name == "Slot1" || gameObject.name == "Slot2"))
+        {
+
+            if ( transform.childCount > 0 && transform.GetChild(0).name == "Image 1")
+            {
+                HeroControlls.instance.upgradeHack = true;
+
+            }
+
+
+
+        }
+         if (gameObject.name == "Slot3" || gameObject.name == "Slot4" || gameObject.name == "Slot5")
+        {
+            if (transform.childCount > 0)
+            {
+                if (transform.GetChild(0).name == "Image 1")
+                    HeroControlls.instance.upgradeHack = false;
+            }
+        }
+
+        //Upgrade Jump
+
+        if (transform.childCount > 0 && (gameObject.name == "Slot1" || gameObject.name == "Slot2"))
+        {
+
+            if (transform.childCount > 0 && transform.GetChild(0).name == "Image 2")
+            {
+                HeroControlls.instance.upgradeJump = true;
+
+            }
+
+
+        }
+         if (gameObject.name == "Slot3" || gameObject.name == "Slot4" || gameObject.name == "Slot5")
+        {
+            if (transform.childCount > 0)
+            {
+
+
+                if (transform.childCount > 0 && transform.GetChild(0).name == "Image 2")
+                    HeroControlls.instance.upgradeJump = false;
+            }
+
+        }
+
+        Debug.Log("Jump " + HeroControlls.instance.upgradeJump);
+        Debug.Log("Hack " +HeroControlls.instance.upgradeHack);
+        Debug.Log("Fight" + HeroControlls.instance.upgradeFight);
+        
     }
 }
